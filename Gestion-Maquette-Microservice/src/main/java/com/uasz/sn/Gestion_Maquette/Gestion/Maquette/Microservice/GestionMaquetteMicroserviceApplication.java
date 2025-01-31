1b995fe77d6e2545a7e0f5e919113b274f5a8f97
ablayefaye04 <fayeablaye404@gmail.com>
2025-01-31 21:57:50 +0000

    l'ajoutes du projet sur maquette

---
+++ Gestion-Maquette-Microservice/src/main/java/com/uasz/sn/Gestion_Maquette/Gestion/Maquette/Microservice/GestionMaquetteMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.uasz.sn.Gestion_Maquette.Gestion.Maquette.Microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionMaquetteMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionMaquetteMicroserviceApplication.class, args);
+	}
+
+}
---
+++ Gestion-Maquette-Microservice/src/test/java/com/uasz/sn/Gestion_Maquette/Gestion/Maquette/Microservice/GestionMaquetteMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uasz.sn.Gestion_Maquette.Gestion.Maquette.Microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionMaquetteMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
